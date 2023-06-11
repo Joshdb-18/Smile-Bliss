3432e5f03320467d89a07c9ca8f79c8f9c9a8934
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-06-11 10:35:04 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Function to handle checkbox behavior
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function () {
+    checkboxes.forEach(cb => {
+      if (cb !== this) {
+        cb.checked = false;
+      }
+    });
+  });
+});
+
+$(function() {
+  $(document).on({
+    mouseover: function(event) {
+      $(this).find('.far').addClass('star-over');
+      $(this).prevAll().find('.far').addClass('star-over');
+    },
+    mouseleave: function(event) {
+      $(this).find('.far').removeClass('star-over');
+      $(this).prevAll().find('.far').removeClass('star-over');
+    }
+  }, '.rate');
+
+  $(document).on('click', '.rate', function() {
+    if (!$(this).find('.star').hasClass('rate-active')) {
+      $(this).siblings().find('.star').addClass('far').removeClass('fas rate-active');
+      $(this).find('.star').addClass('rate-active fas').removeClass('far star-over');
+      $(this).prevAll().find('.star').addClass('fas').removeClass('far star-over');
+    } else {
+      console.log('has');
+    }
+  });
+});
